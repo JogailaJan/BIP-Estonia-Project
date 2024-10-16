44e7a4885f98ce71fd7bada18b1c047ec63450fc
JogailaJan <jogaila.janusas@gmail.com>
2024-10-16 11:55:09 +0300

    Backup, just in case

--- categories_data.py
+++ categories_data.py
@@ -1,5 +1,6 @@
 # categories_data.py
 
+# We will build the categories based on the extracted elements from the PDF
 categories_info = {
     'Pumps': ['Centrifugal Pump', 'Reciprocating Pump', 'Diaphragm Pump'],
     'Tanks': ['Atmospheric Tank', 'Pressurized Tank', 'Underground Tank'],
